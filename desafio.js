bb33f219a03f08389bb1b3d7df519c83d67cded6
Jéssica <160645996+jjessic@users.noreply.github.com>
2024-03-17 01:57:09 +0000

    .

---
+++ desafio.js
@@ -0,0 +1,18 @@
+const aluno = "Jéssica";
+
+const nota1 = 4;
+const nota2 = 6;
+const nota3 = 7;
+
+const mediaFinal = (nota1 + nota2 + nota3) / 3;
+
+if (mediaFinal >= 7) {
+    console.log(
+        `O aluno(a) ${aluno} foi aprovado com uma média final de: ${mediaFinal.toFixed(0)}`);
+} else if (mediaFinal >= 5 && mediaFinal <= 6) {
+    console.log(
+        `O aluno(a) ${aluno} está de recuperação com uma média final de: ${mediaFinal.toFixed(0)}`);
+} else {
+    console.log(
+        `O aluno(a) ${aluno} foi reprovado com uma média final de: ${mediaFinal.toFixed(0)}`);
+}
