9274dcad7269c1543c398bb08bccf245a9ba3e8b
Jéssica <160645996+jjessic@users.noreply.github.com>
2024-03-18 19:42:44 +0000

    js

--- atividade2.js
+++ atividade2.js
@@ -7,19 +7,19 @@ if (imc < 18,5) {
     window.alert(`${nome}, seu nível de IMC é de: ${imc.toFixed(1)}. 
     Portanto, você está abaixo do peso.`)
 }
-else if (imc < 24,9) {
+else if (imc < 25) {
     window.alert(`${nome}, seu nível de IMC é de: ${imc.toFixed(1)}. 
     Portanto, você está no peso ideal.`)
 }
-else if (imc < 29,9) {
+else if (imc < 30) {
     window.alert(`${nome}, seu nível de IMC é de: ${imc.toFixed(1)}. 
     Portanto, você está com sobrepeso.`)
 }
-else if (imc < 34,9) {
+else if (imc < 35) {
     window.alert(`${nome}, seu nível de IMC é de: ${imc.toFixed(1)}. 
     Portanto, você está com obesidade grau 1.`)
 }
-else if (imc < 39,9) {
+else if (imc < 40) {
     window.alert(`${nome}, seu nível de IMC é de: ${imc.toFixed(1)}. 
     Portanto, você está com obesidade grau 2.`)
 }
